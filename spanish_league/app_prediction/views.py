80b96cd22b9d99a30726830ba086f4f187eabf97
Brighht <bright.andohh@gmail.com>
2024-09-15 21:55:50 -0500

    update_02

---
+++ spanish_league/app_prediction/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render
+from spanish_league import settings
+import requests 
+
+# Create your views here.
+def home(request):
+    url = 'https://api.football-data.org/v4/competitions/PD/standings'
+    api_key = settings.API_FOOTBALL_KEY
+    headers = { 'X-Auth-Token': api_key}
+
+    response = requests.get(url, headers=headers)
+
+    if response.status_code == 200:
+        data = response.json()
+        standings = data['standings'][0]['table']
+    return render(request, 'home.html',{'standings':standings})
+
+from django.test import TestCase, Client
+from django.urls import reverse
+
+
