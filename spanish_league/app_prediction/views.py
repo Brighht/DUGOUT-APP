b934ae1e002fd675bdcbec5aa7f0f12c42dff08e
Bright <bright.andohh@gmail.com>
2024-10-01 16:09:24 -0500

    13_4

--- spanish_league/app_prediction/views.py
+++ spanish_league/app_prediction/views.py
@@ -26,5 +26,6 @@ def live_matches(request):
     #checking to see if we really have received the expected data from the data
     if response.status_code == 200:
         data_II = response.json()
-        liveMatches = data_II["matches"][0]                                                                     #retrieves matches diction from the json file 
+        for match in data_II["matches"]:                                  #This iterates throught the json file and locates the values contained by the key "matches"
+            liveMatches = match                                                                     
     return render(request, 'live.html', {"liveMatches": liveMatches},)
